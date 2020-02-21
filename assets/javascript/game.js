ad849fca092b1374575f84ad57cc016e6abadcca
AshfoMon <ashfordmonte@gmail.com>
2020-02-21 16:08:13 -0600

    Game is working, need to implement better css and overall style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,21 @@
 $(document).ready(function () {
 
   var wins = 0;
+  $("#wins").text(wins);
   var losses = 0;
+  $("#losses").text(losses);
   var totalScore = 0;
-  $(".total-score").text(totalScore);
+  $("#total-score").text(totalScore);
 
   var targetNum = Math.floor(50 + Math.random() * 80);
   $("#target-num").text(targetNum);
-  console.log(targetNum)
 
   var gem1 = $("#gem1");
   var gem2 = $("#gem2");
   var gem3 = $("#gem3");
   var gem4 = $("#gem4");
 
-  gem1.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-
-  gem2.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-  while (gem2.attr("data-crystalvalue") === gem1.attr("data-crystalvalue")) {
-    gem2.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-  }
-
-  gem3.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-  while (gem3.attr("data-crystalvalue") === gem1.attr("data-crystalvalue")
-    || gem3.attr("data-crystalvalue") === gem2.attr("data-crystalvalue")) {
-    gem3.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-  }
-
-  gem4.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-  while (gem4.attr("data-crystalvalue") === gem1.attr("data-crystalvalue")
-    || gem4.attr("data-crystalvalue") === gem2.attr("data-crystalvalue")
-    || gem4.attr("data-crystalvalue") === gem3.attr("data-crystalvalue")) {
-    gem4.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
-  }
-
+  gameSetup();
 
   $(gem1).on("click", function () {
     var crystalValue = $(this).attr("data-crystalvalue");
@@ -41,8 +23,18 @@ $(document).ready(function () {
     crystalValue = parseInt(crystalValue);
     totalScore += crystalValue;
 
-    $(".total-score").text("");
-    $(".total-score").text(totalScore);
+    $("#total-score").text(totalScore);
+
+    if (totalScore === targetNum) {
+      wins++;
+      $("#wins").text(wins);
+      gameSetup();
+    }
+    else if (totalScore > targetNum) {
+      losses++;
+      $("#losses").text(losses);
+      gameSetup();
+    }
   });
   $(gem2).on("click", function () {
     var crystalValue = $(this).attr("data-crystalvalue");
@@ -51,7 +43,18 @@ $(document).ready(function () {
     totalScore += crystalValue;
 
 
-    $(".total-score").text(totalScore);
+    $("#total-score").text(totalScore);
+
+    if (totalScore === targetNum) {
+      wins++;
+      $("#wins").text(wins);
+      gameSetup();
+    }
+    else if (totalScore > targetNum) {
+      losses++;
+      $("#losses").text(losses);
+      gameSetup();
+    }
   });
   $(gem3).on("click", function () {
     var crystalValue = $(this).attr("data-crystalvalue");
@@ -59,7 +62,18 @@ $(document).ready(function () {
     crystalValue = parseInt(crystalValue);
     totalScore += crystalValue;
 
-    $(".total-score").text(totalScore);
+    $("#total-score").text(totalScore);
+
+    if (totalScore === targetNum) {
+      wins++;
+      $("#wins").text(wins);
+      gameSetup();
+    }
+    else if (totalScore > targetNum) {
+      losses++;
+      $("#losses").text(losses);
+      gameSetup();
+    }
   });
   $(gem4).on("click", function () {
     var crystalValue = $(this).attr("data-crystalvalue");
@@ -67,12 +81,49 @@ $(document).ready(function () {
     crystalValue = parseInt(crystalValue);
     totalScore += crystalValue;
 
-    $(".total-score").text(totalScore);
+    $("#total-score").text(totalScore);
+
+    if (totalScore === targetNum) {
+      wins++;
+      $("#wins").text(wins);
+      gameSetup();
+    }
+    else if (totalScore > targetNum) {
+      losses++;
+      $("#losses").text(losses);
+      gameSetup();
+    }
   });
 
 
+  function gameSetup() {
+    gem1.attr("data-crystalvalue", Math.floor(1 + Math.random() * 12));
+  
+    gem2.attr("data-crystalvalue", Math.floor(1 + Math.random() * 12));
+    while (gem2.attr("data-crystalvalue") === gem1.attr("data-crystalvalue")) {
+      gem2.attr("data-crystalvalue", Math.floor(1 + Math.random() * 15));
+    }
+  
+    gem3.attr("data-crystalvalue", Math.floor(1 + Math.random() * 12));
+    while (gem3.attr("data-crystalvalue") === gem1.attr("data-crystalvalue")
+      || gem3.attr("data-crystalvalue") === gem2.attr("data-crystalvalue")) {
+      gem3.attr("data-crystalvalue", Math.floor(1 + Math.random() * 12));
+    }
+  
+    gem4.attr("data-crystalvalue", Math.floor(1 + Math.random() * 12));
+    while (gem4.attr("data-crystalvalue") === gem1.attr("data-crystalvalue")
+      || gem4.attr("data-crystalvalue") === gem2.attr("data-crystalvalue")
+      || gem4.attr("data-crystalvalue") === gem3.attr("data-crystalvalue")) {
+      gem4.attr("data-crystalvalue", Math.floor(1 + Math.random() * 12));
+    }
 
-
+    targetNum = Math.floor(50 + Math.random() * 80);
+    $("#target-num").text(targetNum);
+  
+    totalScore = 0;
+    $("#total-score").text(totalScore);
+  }
 
 
 });
+
